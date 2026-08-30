97da781aa78c0cee80531a592ec200e0b05f596f
Sandronod <86520696+Sandronod@users.noreply.github.com>
2026-08-30 22:07:49 +0400

    Set exact 3px gap between association logo and caption text

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -7,9 +7,9 @@
         @if($isHome ?? false)
             <div class="flex items-center justify-center gap-[50px] mb-6">
                 <img src="{{ asset('imgs/Logo_of_the_Supreme_Court_of_Georgia.png') }}" alt="" class="h-[120px] w-auto">
-                <div class="flex flex-col items-center gap-0">
+                <div class="flex flex-col items-center gap-[3px]">
                     <span class="font-heading uppercase tracking-wide text-black text-xs text-center leading-tight">საქართველოს მოსამართლეთა ასოციაცია</span>
-                    <img src="{{ asset('imgs/frontLogo2trans.png') }}" alt="" class="h-[100px] w-auto -my-2">
+                    <img src="{{ asset('imgs/frontLogo2trans.png') }}" alt="" class="h-[100px] w-auto">
                     <span class="font-heading uppercase tracking-wide text-black text-xs text-center leading-tight">Association of Judges of Georgia</span>
                 </div>
             </div>
