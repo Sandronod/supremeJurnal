1a937da0d820b0d0f2b19a263ea83e06143be362
Sandronod <86520696+Sandronod@users.noreply.github.com>
2026-08-31 18:50:53 +0400

    Shrink association emblem logo from 90px to 85px

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -9,7 +9,7 @@
                 <img src="{{ asset('imgs/Logo_of_the_Supreme_Court_of_Georgia.png') }}" alt="" class="h-[120px] w-auto">
                 <div class="flex flex-col items-center">
                     <span class="font-heading uppercase tracking-wide text-black text-[10px] text-center leading-tight mb-[2px]">საქართველოს მოსამართლეთა ასოციაცია</span>
-                    <img src="{{ asset('imgs/frontLogo2trans.png') }}" alt="" class="h-[90px] w-auto">
+                    <img src="{{ asset('imgs/frontLogo2trans.png') }}" alt="" class="h-[85px] w-auto">
                     <span class="font-heading uppercase tracking-wide text-black text-[10px] text-center leading-tight mt-[3px]">Association of Judges of Georgia</span>
                 </div>
             </div>
